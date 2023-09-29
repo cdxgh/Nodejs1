dbd23b8ede7ea970008c5e4e8b0ea06488630ef3
CodeSandbox <support@codesandbox.io>
2023-09-29 05:46:40 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello CodeSandbox");
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res)=>{
+    const log = `${Date.now()}: New request received\n`;
+    fs.appendFile("log.txt",log, (err,data)=>{
+        res.end("Hello from Server Again");
+    });
+  
+});
+myServer.listen(8000, ()=> console.log("Server Started"));
+
+
