e5887b8a1d25570082b813422605a5eaa05754bd
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-09-29 14:27:11 +0000

    Update 6 files

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
-const myServer = http.createServer((req, res)=>{
-    const log = `${Date.now()}: New request received\n`;
-    fs.appendFile("log.txt",log, (err,data)=>{
-        res.end("Hello from Server Again");
-    });
-  
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}:${req.url} New request received\n`;
+  const myUrl = url.parse(req.url);
+  console.log(myUrl);
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end("HEY! I am Abdul Quadir");
+        break;
+      default:
+        res.end("404 Not Found!");
+    }
+  });
 });
-myServer.listen(8000, ()=> console.log("Server Started"));
-
-
+myServer.listen(8000, () => console.log("Server Started"));
