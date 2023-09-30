64684085e60dfbaea5814df5bba7d8990c508e20
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-09-30 15:46:41 +0000

    Understand Express in Nodejs

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
-
-const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
-  const myUrl = url.parse(req.url);
-
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch (myUrl.pathname) {
-      case "/":
-       if(req.method === "GET") res.end("HomePage");
-        break;
-      case "/about":
-        res.end("HEY! I am Abdul Quadir");
-        break;
-        case "/signup":
-          if(req.method === 'GET') res.end('This is a signup form');
-          else if(req.method==='POST'){
-            res.end("Sucess");
-          }
-      default:
-        res.end("404 Not Found!");
-    }
-  });
+const express = require("express");
+const app = express();
+app.get('/', (req, res)=>{
+  return res.send('Hello From Homepage !');
 });
+app.get("/about",(req, res)=>{
+  return res.send(`Hello! ${req.query.name}`);
+});
+const myServer = http.createServer(app);
+
 myServer.listen(8000, () => console.log("Server Started"));
